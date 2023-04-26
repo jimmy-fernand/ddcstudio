1c9fd69db18b6fd96b0242c6f6c984758a25adca
Fernand Jimmy <jijifernand@gmail.com>
2023-04-26 10:27:20 +0200

    database folder commit

---
+++ database/migrations/2022_12_15_095609_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("categorie_id")->index();
+            $table->string('title');
+            $table->text('description');
+            $table->boolean("finished")->default(0);
+            $table->foreign("categorie_id")->references("id")->on("categories");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
